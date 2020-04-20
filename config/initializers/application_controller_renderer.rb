36ba5c8c425bd1645da00310f17b7265e47afddb
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-04-20 21:02:29 +0900

    [Finish]3-5([Add]PostImage Show&Index)

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e940d3a251a176c322cfe00636872b37b5650bb1139872e80ef6b3c96aa3d4ed3497c0c48b70f3d1f28640912d844f6ca591160331db17d9fc512b98b019d18f'
